1256d2fdcbcc0f3c35bb989eb3fc4e884ed269b1
Gosuke Miyashita <gosukenator@gmail.com>
2011-08-16 12:19:03 -0400

    rewrite network settings after cloning image

--- maglica/host_worker/vm.py
+++ maglica/host_worker/vm.py
@@ -1,10 +1,12 @@
 import subprocess
 import socket
+import guestfs
+import re
 
 def create(args):
-    image = args["image"]
-    hostname = args["hostname"]
-    
+    image      = args["image"]
+    hostname   = args["hostname"]
+    image_file = "/var/lib/libvirt/images/" + hostname + ".img"
     cmdline = [
         "virt-clone",
         "-o",
@@ -12,8 +14,37 @@ def create(args):
         "-n",
         hostname,
         "-f",
-        "/var/lib/libvirt/images/" + hostname + ".img",
+        image_file,
         ]
     subprocess.call(cmdline)
+
+    g = guestfs.GuestFS()
+    g.add_drive('/var/lib/libvirt/images/test.mizzy.org.img')
+    g.launch()
+    filesystems = g.list_filesystems()
+    for filesystem in filesystems:
+        if re.match(r'.+root$', filesystem[0]):
+            fs = filesystem[0]
+
+    g.mount(fs, '/')
+
+    ifcfg='''DEVICE=%s
+BOOTPROTO=dhcp
+ONBOOT=yes
+TYPE="Ethernet"
+DHCP_HOSTNAME=%s
+'''
+
+    network='''NETWORKING=yes
+HOSTNAME=%s'''
+
+    ifcfg0  = ifcfg % ('eth0', hostname)
+    network = network % ( hostname )
+
+    g.write_file('/etc/sysconfig/network-scripts/ifcfg-eth0', ifcfg0, 0)
+    g.write_file('/etc/sysconfig/network', network, 0)
+    g.write_file('/etc/udev/rules.d/70-persistent-net.rules', '', 0)
+    g.sync()
+    g.umount_all()
 
     return "Created " + hostname + " successfully on " + socket.gethostname()

0d3225058a431112e1c1cca202a2d2a773dc4aa4
Gosuke Miyashita <gosukenator@gmail.com>
2011-08-18 14:52:43 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name = "Maglica",
+    version = "0.1",
+    packages = find_packages(),
+    scripts = [
+      'scripts/maglica',
+      'scripts/maglica_client_worker',
+      'scripts/maglica_host_worker',
+    ],
+    author = "Gosuke Miyashita",
+    author_email = "gosukenator@gmail.com",
+    description = "Simple internal cloud tool",
+    url = "https://github.com/mizzy/maglica",
+)

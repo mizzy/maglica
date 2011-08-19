8b36240424d2a8d0cf609d9dfa728fc3bc228089
Gosuke Miyashita <gosukenator@gmail.com>
2011-08-19 18:57:39 +0900

    implement vm list, stop, start

--- maglica/cli/vm.py
+++ maglica/cli/vm.py
@@ -3,3 +3,18 @@ import maglica.vm
 def clone(args):
     maglica.vm.clone(args)
     
+def list():
+    print "Name                               Host                 State"
+    print "------------------------------------------------------------------------"
+
+    vms = maglica.vm.list()
+    for vm in vms:
+        print "%-35s %-20s %-10s" % ( vm["name"], vm["host"], vm["state"] )
+
+def start(args):
+    maglica.vm.start(args)
+    print "%s started" % ( args["name"] )
+
+def stop(args):
+    maglica.vm.stop(args)
+    print "%s is being shutdown" % ( args["name"] )
--- maglica/vm.py
+++ maglica/vm.py
@@ -1,5 +1,8 @@
 import maglica.dispatcher
 import maglica.image
+import libvirt
+import maglica.config
+import re
 
 def clone(args): 
     images = maglica.image.list()
@@ -14,4 +17,67 @@ def clone(args):
         "host"   : host,
         "args"   : args,
         })
-    
+
+def list():
+    vms = []
+    for vm in get_active_domains():
+        vms.append(vm)
+    for vm in get_inactive_domains():
+        vms.append(vm)
+    return vms
+
+def start(args):
+    dom = get_inactive_domain(args["name"])
+    dom.create()
+
+def stop(args):
+    dom = get_active_domain(args["name"])
+    dom.shutdown()
+
+def get_active_domains():
+    config = maglica.config.load()
+    vms = []
+    for host in config.hosts:
+        conn = libvirt.open('remote://' + host)
+        ids = conn.listDomainsID()
+        for id in ids:
+            dom = conn.lookupByID(id)
+            vms.append({
+                "name"  : dom.name(),
+                "state" : "running",
+                "host"  : host,
+                })
+    return vms
+
+def get_inactive_domains():
+    config = maglica.config.load()
+    vms = []
+    for host in config.hosts:
+        conn = libvirt.open('remote://' + host)
+        domains = conn.listDefinedDomains()
+        for domain in domains:
+            if not re.match(r'.+[\.\-\_]original$', domain):
+                vms.append({
+                    "name"  : domain,
+                    "state" : "shut off",
+                    "host"  : host,
+                    })
+    return vms
+
+def get_active_domain(name):
+    config = maglica.config.load()
+    for host in config.hosts:
+        conn = libvirt.open('remote://' + host)
+        ids = conn.listDomainsID()
+        for id in ids:
+            dom = conn.lookupByID(id)
+            if name == dom.name():
+                return dom
+
+def get_inactive_domain(name):
+    config = maglica.config.load()
+    for host in config.hosts:
+        conn = libvirt.open('remote://' + host)
+        dom = conn.lookupByName(name)
+        if dom:
+            return dom

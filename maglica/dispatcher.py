e8c5cdcbb8465a08bea06e6c98fdbb23422a1350
Gosuke Miyashita <gosukenator@gmail.com>
2011-10-07 15:31:58 +0900

    yaml returns numbers as int, so convert int to str.

--- maglica/dispatcher.py
+++ maglica/dispatcher.py
@@ -10,7 +10,7 @@ if cfg.client.has_key("rep_port"):
 
 context = zmq.Context()
 requestor = context.socket(zmq.REQ)
-requestor.connect(str("tcp://" + cfg.client["host"] + ":" + rep_port))
+requestor.connect(str("tcp://" + cfg.client["host"] + ":" + str(rep_port)))
 
 def dispatch(args):
     requestor.send( json.dumps(args) )
